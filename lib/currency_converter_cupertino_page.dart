d76ce68067060e8d32b0f86470d58f9cb9072f12
TeeKay <teekay5041@gmail.com>
2025-06-12 21:30:27 +0100

    Add README.md

---
+++ lib/currency_converter_cupertino_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+
+class CurrencyConverterCupertinoPage extends StatefulWidget {
+  const CurrencyConverterCupertinoPage({super.key});
+
+  @override
+  State<CurrencyConverterCupertinoPage> createState() => _CurrencyConverterCupertinoPageState();
+}
+
+class _CurrencyConverterCupertinoPageState extends State<CurrencyConverterCupertinoPage> {
+
+double result = 0;
+ final TextEditingController textEditingController = TextEditingController();
+ 
+  
+ void convert () {
+   setState(() {
+    result = double.parse(textEditingController.text)*1730;
+   });
+ } 
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return CupertinoPageScaffold(
+     backgroundColor: CupertinoColors.systemGrey3,
+      navigationBar:const CupertinoNavigationBar(
+       backgroundColor:CupertinoColors.systemGrey3,
+       middle:Text('Currency Converter'),
+     ),
+     child: Center(
+       child: Padding(
+         padding: const EdgeInsets.all(8.0),
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children : [
+             // int -> string integervalue.tostring();
+             // string ->int int.parse/double.parse();
+              Text(
+               'NGN ${result != 0 ? result.toStringAsFixed(2): result.toStringAsFixed(0)}',
+               style:const TextStyle(
+                 fontSize: 45,
+                 fontWeight: FontWeight.bold,
+                 color: Color.fromARGB(255, 255, 255, 255)
+               ),
+             ),
+             //padding
+             //container
+             CupertinoTextField( 
+              controller: textEditingController,
+               style:  const TextStyle(
+                 color: CupertinoColors.black
+               ),
+                decoration: BoxDecoration(
+                  color: CupertinoColors.white,
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(5), 
+                ),
+                placeholder: 'Pls enter amount in dollar',
+                prefix: const Icon(CupertinoIcons.money_dollar),
+                keyboardType:const TextInputType.numberWithOptions(
+                 decimal:true,
+                 signed:true
+               )
+             ),
+             //button
+         
+             // raised
+             //appears  like a text
+             const SizedBox(height: 10.0,),
+             CupertinoButton( 
+               onPressed: convert,
+               color: CupertinoColors.black,
+               child:const Text('Convert')
+             ),
+           ],
+         ),
+       ),
+     ),
+   );
+ }
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:currency_converter/currency_converter_cupertino_page.dart';
+import 'package:currency_converter/currency_converter_material_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(const MyApp());
+}
+/* types of widget 
+1. StatelessWidget
+2. StatefulWidget*/
+
+// State
+
+/*1. Material design
+  2. Cupertino design*/
+   
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+   @override
+    Widget build(BuildContext context) {
+      return const MaterialApp(
+        home: CurrencyConverterMaterialPage()
+      );
+    }
+  }
+
+  class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CurrencyConverterCupertinoPage()
+   );
+  }
+}
